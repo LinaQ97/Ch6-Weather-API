f6c587a67c102c8bd8a58b0d50c6e6373bf429b0
Lina Quintana <lquintana479@gmail.com>
2023-07-26 21:21:49 -0400

    added bootstrap elements into HTML to design the outlook of the page

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var apiKey = "9c9aaccb9d5d0fd8ba58ed3341a13b50"
+var titleEl = document.getElementById("title")
+var tempEl = document.getElementById("temp")
+var windEl = document.getElementById("wind")
+var humidityEl = document.getElementById("humidity")
+
